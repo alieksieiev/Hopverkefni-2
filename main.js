ac4dd6c275fa2ba5545b1303dbf91cb59b19fd0c
Dagur Hardarson <201810188@post.au.dk>
2021-11-25 02:18:14 +0000

    todo items komin inn, vantar ad styla

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { el } from './lib/helpers.js';
+
 const data = await fetch('./data.json').then(response => {
   return response.json();
 });
@@ -7,3 +9,50 @@ const todoItems = data.items;
 console.log(todoCategories);
 console.log(todoItems);
 
+const main = document.querySelector('main');
+
+function renderTodoItems() {
+  if(todoItems.length === 0) {
+    const noItemsElement = el('p', 'Engin verkefni sett fyrir')
+  }
+
+  const todosElement = el('div');
+  todosElement.classList.add('itemList__list');
+  const sectionElement = el('section', todosElement);
+  sectionElement.classList.add('container-right');
+  main.appendChild(sectionElement);
+
+  for (let i = 0; i < todoItems.length; i += 1) {
+    const item = todoItems[i];
+    const label = el('label', item.title);
+    const p = el('p', item.description);
+    const tags = el('div');
+    const itemDiv = el('div');
+
+    /*
+    if (item.due != null) {
+      const dueDate = new Date(item.due).toString();
+      return dueDate;
+    }
+    */
+
+    const due = el('p', new Date(item.due).toString());
+    const category = el('label', item.category);
+
+    tags.appendChild(due);
+    tags.appendChild(category);
+
+    itemDiv.classList.add('todo__item');
+    itemDiv.appendChild(label);
+    itemDiv.appendChild(p);
+    itemDiv.appendChild(tags);
+
+    todosElement.appendChild(itemDiv);
+  }
+}
+
+for (const item of todoItems) {
+  console.log(item.id);
+}
+
+renderTodoItems();
